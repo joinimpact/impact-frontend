0481c8a2894186499654998148ce3744288a3b11
Denis Kozik <denis.kozik@studiomobile.ru>
2020-06-29 18:55:32 +0300

    - change logo style and add link to login page

--- src/modules/Auth/view/components/AuthLayout/AuthLayout.tsx
+++ src/modules/Auth/view/components/AuthLayout/AuthLayout.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import block from 'bem-cn';
-import { Logo } from 'shared/view/elements';
+import { Link, Logo } from 'shared/view/elements';
 import { i18nConnect, ITranslateProps } from 'services/i18n';
+import routes from 'modules/routes';
 
 import './AuthLayout.scss';
 
@@ -15,17 +16,23 @@ type TProps = IOwnProps & ITranslateProps;
 
 class AuthLayout extends React.PureComponent<TProps> {
   public render() {
-    const { translate: t, withoutLogo } = this.props;
+    const { translateArray: ta, withoutLogo } = this.props;
     return (
       <div className={b()}>
         <div className={b('content', { 'without-logo': withoutLogo })}>
           {!withoutLogo && (
             <div className={b('content-left')}>
-              <div className={b('logo')}>
-                <Logo size={70}/>
-              </div>
+              <Link href={routes.auth.login.getPath()}>
+                <div className={b('logo')}>
+                  <Logo size={70}/>
+                </div>
+              </Link>
               <div className={b('name')}>
-                {t('LOGIN-FORM:STATIC:TEXT')}
+                {ta('AUTH-LAYOUT:STATIC:TEXT').map(row => (
+                  <div className={b('name-row')}>
+                    {row}
+                  </div>
+                ))}
               </div>
             </div>
           )}
