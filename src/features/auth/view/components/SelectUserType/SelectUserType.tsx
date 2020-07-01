d4ebebc7c338d7486fa06bd6da20512df1f524d1
Denis Kozik <denis.kozik@studiomobile.ru>
2020-07-01 15:03:47 +0300

    - change select user type layout

--- src/features/auth/view/components/SelectUserType/SelectUserType.tsx
+++ src/features/auth/view/components/SelectUserType/SelectUserType.tsx
@@ -3,7 +3,7 @@ import block from 'bem-cn';
 import { bind } from 'decko';
 import { i18nConnect, ITranslateProps } from 'services/i18n';
 import { TUserType } from 'shared/types/app';
-import { Button } from 'shared/view/elements';
+import { Button, Radio } from 'shared/view/elements';
 
 import './SelectUserType.scss';
 
@@ -11,24 +11,58 @@ interface IOwnProps {
   onUserTypeSelected(userType: TUserType): void;
 }
 
+interface IState {
+  userType: TUserType | null;
+}
+
 const b = block('select-user-type');
 
 type TProps = IOwnProps & ITranslateProps;
 
-class SelectUserType extends React.PureComponent<TProps> {
+class SelectUserType extends React.PureComponent<TProps, IState> {
+  public state: IState = {
+    userType: null,
+  };
+
   public render() {
     const { translate: t } = this.props;
+    const { userType } = this.state;
     return (
       <div className={b()}>
         <div className={b('caption')}>
           {t('SIGN-UP-FORM-CONTAINER:STATIC:SELECT-USER-TYPE-CAPTION')}
         </div>
-        <div className={b('row')}>
-          <Button color="blue" onClick={this.handleSelectUserType.bind(this, 'volunteer')}>
-            {t('SIGN-UP-FORM-CONTAINER:STATIC:VOLUNTEER-TYPE').toUpperCase()}
-          </Button>
-          <Button color="blue" onClick={this.handleSelectUserType.bind(this, 'npo')}>
-            {t('SIGN-UP-FORM-CONTAINER:STATIC:NONPROFIT-TYPE').toUpperCase()}
+        <div className={b('cards')}>
+          <div className={b('card')} onClick={this.handleSelectUserType.bind(this, 'volunteer')}>
+            <div className={b('card-title')}>{t('SELECT-USER-TYPE:CAPTION:VOLUNTEER')}</div>
+            <div className={b('card-body')}>{t('SELECT-USER-TYPE:STATIC:VOLUNTEER-TEXT')}</div>
+            <div className={b('card-action')}>
+              <Radio
+                checked={userType === 'volunteer'}
+                type="circle"
+                name={'select-user-type'}
+                label={t('SELECT-USER-TYPE:STATIC:I-AM-A-VOLUNTEER')}
+                onChange={this.handleSelectUserType.bind(this, 'volunteer')}
+              />
+            </div>
+          </div>
+          <div className={b('card')} onClick={this.handleSelectUserType.bind(this, 'npo')}>
+            <div className={b('card-title')}>{t('SELECT-USER-TYPE:CAPTION:ORGANIZATION')}</div>
+            <div className={b('card-body')}>{t('SELECT-USER-TYPE:STATIC:ORGANIZATION-TEXT')}</div>
+            <div className={b('card-action')}>
+              <Radio
+                checked={userType === 'npo'}
+                type="circle"
+                name={'select-user-type'}
+                label={t('SELECT-USER-TYPE:STATIC:I-AM-AN-ORGANIZATION')}
+                onChange={this.handleSelectUserType.bind(this, 'npo')}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={b('actions')}>
+          <Button color="blue" disabled={userType == null} onClick={this.handleContinue}>
+            {t('SHARED:BUTTONS:CONTINUE')}
           </Button>
         </div>
       </div>
@@ -37,7 +71,15 @@ class SelectUserType extends React.PureComponent<TProps> {
 
   @bind
   private handleSelectUserType(userType: TUserType) {
-    this.props.onUserTypeSelected(userType);
+    this.setState({
+       userType,
+    });
+    // this.props.onUserTypeSelected(userType);
+  }
+
+  @bind
+  private handleContinue() {
+    this.props.onUserTypeSelected(this.state.userType!);
   }
 }
 
--- src/shared/view/elements/index.ts
+++ src/shared/view/elements/index.ts
@@ -8,3 +8,4 @@ export { default as Logo } from './Logo/Logo';
 export { default as Link } from './Link/Link';
 export { default as Menu } from './Menu/Menu';
 export { default as Select, ISelectProps } from './Select/Select';
+export { default as Radio } from './Radio/Radio';
