8ec2b9f918366f5a9cd38e24bbd9f47864822e02
Denis Kozik <denis.kozik@studiomobile.ru>
2020-08-31 16:14:35 +0300

    - reset state when logout

--- src/services/npo/redux/reducers/index.ts
+++ src/services/npo/redux/reducers/index.ts
@@ -2,8 +2,15 @@ import * as NS from '../../namespace';
 import { combineReducers } from 'redux';
 import dataReducer from './data';
 import communicationReducers from './communication';
+import initial from '../initial';
+import makeResetStateReducer from 'shared/redux/makeResetStateReducer';
+import { IResetAppStateAction } from 'shared/redux/actions';
+import { composeReducers } from 'shared/util/redux';
 
-export default combineReducers<NS.IReduxState>({
+const baseReducer = combineReducers<NS.IReduxState>({
   communications: communicationReducers,
   data: dataReducer,
 });
+
+const resetReducer = makeResetStateReducer<IResetAppStateAction, NS.IReduxState>('APP:RESET', initial);
+export default composeReducers([baseReducer, resetReducer]);
--- src/services/npoChat/redux/reducers/index.ts
+++ src/services/npoChat/redux/reducers/index.ts
@@ -1,11 +1,18 @@
 import { combineReducers } from 'redux';
 import * as NS from '../../namespace';
+import initial from '../initial';
 
 import { ReducersMap } from 'shared/types/redux';
 import communicationReducer from './communication';
 import dataReducer from './data';
+import makeResetStateReducer from 'shared/redux/makeResetStateReducer';
+import { IResetAppStateAction } from 'shared/redux/actions';
+import { composeReducers } from 'shared/util/redux';
 
-export default combineReducers<NS.IReduxState>({
+const baseReducer = combineReducers<NS.IReduxState>({
   data: dataReducer,
   communications: communicationReducer,
 } as ReducersMap<NS.IReduxState>);
+
+const resetReducer = makeResetStateReducer<IResetAppStateAction, NS.IReduxState>('APP:RESET', initial);
+export default composeReducers([baseReducer, resetReducer]);
