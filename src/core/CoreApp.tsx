169df343e9e5ad1f017fd88851584f187a062fa0
Tyler Lafayette <tylerlafayette@gmail.com>
2020-09-03 14:28:00 -0700

    Cleanup linting, fix type in CoreApp

--- src/core/CoreApp.tsx
+++ src/core/CoreApp.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { Switch } from 'react-router';
@@ -6,19 +6,17 @@ import { Router } from 'react-router-dom';
 import { IAppData } from 'shared/types/app';
 import createRoutes from './routes';
 
-/* tslint:disable:function-name */
-function CoreApp({ appModules, store, history }: IAppData) {
-  return (
-    <Provider store={store}>
-      <Router history={history}>
-        <ConnectedRouter history={history}>
-          <Switch>
-            {createRoutes(appModules)}
-          </Switch>
-        </ConnectedRouter>
-      </Router>
-    </Provider>
-  );
-}
+/* eslint-disable @typescript-eslint/naming-convention */
+const CoreApp: React.FC<IAppData> = ({ appModules, store, history }: IAppData): ReactElement => {
+	return (
+		<Provider store={store}>
+			<Router history={history}>
+				<ConnectedRouter history={history}>
+					<Switch>{createRoutes(appModules)}</Switch>
+				</ConnectedRouter>
+			</Router>
+		</Provider>
+	);
+};
 
 export default CoreApp;
